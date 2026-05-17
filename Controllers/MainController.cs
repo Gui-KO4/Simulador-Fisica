5adf9bedbddd9d5fdfddc252771838be47d7fe0d
Leo <leandros29.personal@gmail.com>
2026-05-17 03:49:16 +0100

    Fix

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -87,10 +87,6 @@ public class MainController
                     }
                     simulationController.SMD(parts[1], parts[2], parts[3]);
                     break;
-                case "TEST":
-                    SimulationTests tests = new SimulationTests(projectController, particleController);
-                    tests.ExecuteTests();
-                    break;
                 case "Exit":
                     return;
                 case "Tests":
